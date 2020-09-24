40777396c1433970d16a80c1aaa98c54140a0bc3
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2020-09-24 08:35:19 +0000

    add fields

--- blog/models.py
+++ blog/models.py
@@ -2,17 +2,40 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
+    is_public = models.BooleanField(default=False)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def save(self, *args, **kwargs):
+        if self.is_public and not self.published_date:
+            self.published_date = timezone.now()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
