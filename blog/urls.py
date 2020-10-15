548d7bed08a0aaf342ce4627f366f5a06840d0b9
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2020-10-15 02:32:19 +0000

    formを作成したやよ～

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import IndexView, PostDetailView, CategoryListView, TagListView
+from .views import IndexView, PostDetailView, PostNew, CategoryListView, TagListView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostNew.as_view(), name='post_new'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
     path('tags/', TagListView.as_view(), name='tag_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render
 
 from django.db.models import Count, Q
 from django.http import Http404
+from django.utils import timezone
+from django.shortcuts import redirect
+from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from blog.models import Post, Category, Tag
+from blog.forms import PostForm
 
 
 # Create your views here.
+class PostNew(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_edit.html"
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.created_date = timezone.now()
+        post.save()
+        return redirect('/')
 
 class PostDetailView(DetailView):
     model = Post
